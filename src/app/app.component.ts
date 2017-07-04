982fd2ef05e6641d9d83280d398ace434181429b
Jonathan Kalambay <jonnyk20@gmail.com>
2017-07-04 22:51:31 +0200

    Finished Basics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,23 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styles: [`
   h3 {color: dodgerblue;}
-  
+  .white {color: white}
   `
   ]
 })
 export class AppComponent {
   title = 'app';
+  display = false;
+  clicks = [];
+  counter =0;
+  displayDetails() {
+    if (this.display == true)
+    {this.display = false; }
+    else {this.display = true; }
+    this.counter++;
+    this.clicks.push(this.counter);
+    
+    
+  }
+ 
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer = false;
-  ServerCreationStatus = 'No server was created';
+  allowNewUser = false;
+  ServerCreationStatus = 'No server was created.';
+  ServerName = 'TestServer';
+  UserName = '';
+  serverCreated = false;
+  servers = ['Testserver','Testserver 2']
+
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
@@ -15,6 +21,31 @@ export class ServersComponent implements OnInit {
    }
 
   ngOnInit() {
+  }
+  onCreateServer(){
+    this.serverCreated = true;
+    this.servers.push(this.ServerName)
+    this.ServerCreationStatus = 'Server was created. Name is ' +this.ServerName;
+  }
+  onUpdateServerName(event: Event)
+  {
+    this.ServerName = (<HTMLInputElement>event.target).value;
+  }
+
+  onUpdateUserName(event){
+      this.UserName = (<HTMLInputElement>event.target).value;
+      var input = this.UserName;
+      if (input.length){
+        this.allowNewUser = true;
+      }
+      else {
+        this.allowNewUser = false;
+      }
+  }
+
+  newUser(event)
+  {
+    this.UserName = '';
   }
 
 }
