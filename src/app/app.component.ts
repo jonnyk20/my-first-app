8343332acb8afd6dece14c290185e278b9c74bda
jonnyk20 <jonnyk20@gmail.com>
2017-07-05 17:16:16 +0200

    Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,25 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styles: [`
-  h3 {color: dodgerblue;}
-  .white {color: white}
-  `
-  ]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
-  display = false;
-  clicks = [];
-  counter =0;
-  displayDetails() {
-    if (this.display == true)
-    {this.display = false; }
-    else {this.display = true; }
-    this.counter++;
-    this.clicks.push(this.counter);
-    
-    
-  }
+  
  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
-import { WarningAlert  } from './warningalert/warningalert.component';
-import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping/shopping-list-edit/shopping-list-edit.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
+import { RecipeComponent } from './recipe/recipe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    WarningAlert,
-    SuccessAlertComponent
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    RecipeComponent
+ 
   ],
   imports: [
     BrowserModule,
